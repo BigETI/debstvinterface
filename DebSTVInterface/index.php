c22142c77b603f1d6d5ee39f65b5e6667d4feddc
Ethem Kurt <ethem-93@hotmail.de>
2016-06-07 12:27:27 +0200

    Implementing features (W.I.P.)

--- DebSTVInterface/index.php
+++ DebSTVInterface/index.php
@@ -1,5 +1,7 @@
 <?php
-if (isset ( $_POST ['cmd'] )) {
-	//
+include_once './includes/DebSTV.php';
+if (isset ( $_POST ['c'], $_POST ['p'] )) {
+	$api = new DebSTV ();
+	echo $api->execute ( $_POST ['c'], $_POST ['p'] );
 }
 ?>
