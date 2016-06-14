a2106c33ca80a5cfacffb5e9d18d260fe93a970c
Ethem Kurt <ethem-93@hotmail.de>
2016-06-14 12:55:57 +0200

    SOme changes to class.

--- DebSTVInterface/index.php
+++ DebSTVInterface/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once './includes/DebSTV.php';
+include_once './includes/debstvhelp.php';
 if (isset ( $_POST ['c'], $_POST ['p'] )) {
 	$api = new DebSTV ();
 	echo $api->execute ( $_POST ['c'], $_POST ['p'] );
